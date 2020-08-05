d3b2a95441265d985e9691d82a853732211f397b
lenaabdu <lenaabdu23@gmail.com>
2020-08-05 14:01:20 +0100

    Updates

---
+++ precache-manifest.53eab7853988bc5b258a031012db2df3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9100ca8c9c18d2af7ec1ca79dfe7b961",
+    "url": "/Social-Site/index.html"
+  },
+  {
+    "revision": "a1fa1f016cc0b6329857",
+    "url": "/Social-Site/static/css/2.7c5c0350.chunk.css"
+  },
+  {
+    "revision": "09515da9e420046e04d2",
+    "url": "/Social-Site/static/css/main.dbd13bac.chunk.css"
+  },
+  {
+    "revision": "a1fa1f016cc0b6329857",
+    "url": "/Social-Site/static/js/2.882ef0b7.chunk.js"
+  },
+  {
+    "revision": "bc645b8808bddaa3c0c8d72dff721b3b",
+    "url": "/Social-Site/static/js/2.882ef0b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09515da9e420046e04d2",
+    "url": "/Social-Site/static/js/main.731389d5.chunk.js"
+  },
+  {
+    "revision": "151becba16ff90ed259b",
+    "url": "/Social-Site/static/js/runtime-main.dacf84d5.js"
+  },
+  {
+    "revision": "27f665386ad309d2b3bcd496aafb5e1c",
+    "url": "/Social-Site/static/media/Social.27f66538.jpg"
+  }
+]);
